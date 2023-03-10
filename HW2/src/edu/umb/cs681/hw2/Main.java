c8a64a207ccdd29555605ab38f8525ed6f9c8570
lahasya <lahasya.lnu001@umb.edu>
2023-03-09 23:56:27 -0500

    changes in main .java

--- HW2/src/edu/umb/cs681/hw2/Main.java
+++ HW2/src/edu/umb/cs681/hw2/Main.java
@@ -1,6 +1,9 @@
 package edu.umb.cs681.hw2;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -13,47 +16,27 @@ public class Main {
         usedCars.add(new Car("Range Rover", "Evoque", 48, 2017, 52000f));
         usedCars.add(new Car("Mazda", "CX-5", 28, 2016, 32000f));
 
-       
-        double lowestCarPrice = usedCars.stream()
-                                .map((car)-> car.get_price()).reduce((float) 0,
-                                (result,price)->{
-                                        if(result == 0){
-                                            result = price;
-                                        }
-                                        else if(price < result){
-                                            result = price;
-                                        }
-                                    return result;
-                                });
-        System.out.println("lowest car price :" + lowestCarPrice);
+        List<Car> carSortByPrice = usedCars.stream()
+                .sorted(Comparator.comparingDouble(Car::get_price)).collect(Collectors.toList());
 
-        double highestCarPrice = usedCars.stream()
-        .map((car)-> car.get_price()).reduce((float) 0,
-        (result,price)->{
-                if(result == 0){
-                    result = price;
-                }
-                else if(price > result){
-                    result = price;
-                }
-            return result;
-        });
+        System.out.println("lowest car price :" + carSortByPrice.get(0).get_price());
 
-        System.out.println("Highest Car price :" + highestCarPrice);
+        System.out.println("Highest Car price :" + carSortByPrice.get(carSortByPrice.size() - 1).get_price());
 
-
-        System.out.println("calculating average price of the car");
+        System.out.println("\ncalculating average price of the car using class instance");
         double averagePrice = usedCars.stream()
                 .map((car) -> car.get_price()).reduce(new CarPriceResultHolder(),
                         (result, price) -> {
-                            //System.out.println(result.getNumCarExamined() + " "+ result.getAverage() +" " + price);
+                            // System.out.println(result.getNumCarExamined() + " "+ result.getAverage() +" "
+                            // + price);
                             CarPriceResultHolder resultHolder = result;
                             int numCarExamined = (int) resultHolder.getNumCarExamined();
                             double average = resultHolder.getAverage();
                             float r = (float) (((numCarExamined * average) + price) / (numCarExamined + 1));
                             resultHolder.setNumCarExamined(numCarExamined + 1);
                             resultHolder.setAverage(r);
-                            //System.out.println(resultHolder.getAverage() + " " + resultHolder.getNumCarExamined());
+                            // System.out.println(resultHolder.getAverage() + " " +
+                            // resultHolder.getNumCarExamined());
                             return resultHolder;
                         },
                         (finalResult, intermediateResult) -> finalResult)
