4ba2026263b9b34ee952c45c31d3b1f839a963cc
lahasya <lahasya.lnu001@umb.edu>
2023-05-16 16:03:32 -0400

    changes in HW4

--- HW14/src/edu/umb/cs681/hw14/Main.java
+++ HW14/src/edu/umb/cs681/hw14/Main.java
@@ -7,13 +7,16 @@ public class Main {
         EntranceHandler entryThread = new EntranceHandler(monitor);
         ExitHandler exitThread = new ExitHandler(monitor);
         StatsHandler statsThread = new StatsHandler(monitor);
+        EntranceHandler entryThread2 = new EntranceHandler(monitor);
+        ExitHandler exitThread2 = new ExitHandler(monitor);
+        StatsHandler statsThread2 = new StatsHandler(monitor);
 
         Thread t1 = new Thread(entryThread);
         Thread t2 = new Thread(exitThread);
         Thread t3 = new Thread(statsThread);
-        Thread t4 = new Thread(entryThread);
-        Thread t5 = new Thread(exitThread);
-        Thread t6 = new Thread(statsThread);
+        Thread t4 = new Thread(entryThread2);
+        Thread t5 = new Thread(exitThread2);
+        Thread t6 = new Thread(statsThread2);
 
         t1.start();
         t4.start();
@@ -33,6 +36,9 @@ public class Main {
         exitThread.setDone();
         statsThread.setDone();
         entryThread.setDone();
+        exitThread2.setDone();
+        statsThread2.setDone();
+        entryThread2.setDone();
 
         t1.interrupt();
         t2.interrupt();
@@ -40,16 +46,6 @@ public class Main {
         t4.interrupt();
         t5.interrupt();
         t6.interrupt(); 
-        try {
-            t1.join();
-            t2.join();
-            t3.join();
-            t4.join();
-            t5.join();
-            t6.join();
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        
     }
 }
