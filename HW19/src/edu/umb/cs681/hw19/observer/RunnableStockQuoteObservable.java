bf9c3d13d331b7c7392f785f66262e8914277abd
lahasya <lahasya.lnu001@umb.edu>
2023-05-17 16:12:40 -0400

    changes in HW19

---
+++ HW19/src/edu/umb/cs681/hw19/observer/RunnableStockQuoteObservable.java
@@ -0,0 +1,43 @@
+package edu.umb.cs681.hw19.observer;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class RunnableStockQuoteObservable implements Runnable {
+    private AtomicBoolean done = new AtomicBoolean(false);
+
+    public void setDone() {
+            done.set(true);
+    }
+
+    @Override
+    public void run() {
+        StockQuoteObservable item = new StockQuoteObservable();
+        while (true) { // for infinite loop
+                if (done.get()) {
+                    System.out.println("As done=true Terminated");
+                    break;
+                }
+
+                TableObserver tableObserver = new TableObserver();
+                item.addObserver(tableObserver);
+                LineChartObserver lineChartObserver = new LineChartObserver();
+                item.addObserver(lineChartObserver);
+                Three_3_DObserver ThreeDObserver = new Three_3_DObserver();
+                item.addObserver(ThreeDObserver);
+                StockEvent a = new StockEvent("crash", 100);
+                item.notifyObservers(a);
+
+                item.changeQuote("jump", 300.0);
+
+                StockEvent b = new StockEvent("jump", 400);
+                item.notifyObservers(b);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.toString());
+                continue;
+            }
+        }
+    }
+}
